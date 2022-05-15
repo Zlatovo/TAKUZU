49e4f48bdc8e260f657d0d520e8b20457a602ad3
Padrien22 <adrien.pouyat@efrei.net>
2022-05-15 22:59:35 +0200

    Revert "presentation du debue du jeux"
    This reverts commit cf3a89ce43a0205feec438568d0b37810461b08b.

--- main.c
+++ main.c
@@ -1,22 +1,5 @@
 #include "TAKUZU.h"
 
 int main() {
-    bool star = true;
-    char commentcer = 'a';
-    printf("Bienvenu au jeux TAKUZU progrmer par Adrien et Luka\n");
-    while (star == true){
-        //si l'utilisateur veux commencer a jouer :
-        while (commentcer != 'y' && commentcer != 'n'){
-            printf("Voulez-vous commencer une partie y/n : ");
-            scanf("%c", &commentcer);
-            printf("\n");
-        }
-        if (commentcer == 'y'){
-            star_game_Takuzu();
-            printf("\n\n\n");
-        } else {
-            star = false;
-        }
-    }
-    printf("Merci d'avoir jouer a notre jeux.\n");
+    star_game_Takuzu();
 }
